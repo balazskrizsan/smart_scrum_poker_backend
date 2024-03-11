1ea83964fd8494d0b00e8a8bfd195da8433767c2
Balazs Krizsan <krizsan.balazs@gmail.com>
2024-03-11 22:48:48 +0100

    init

---
+++ src/main/java/com/kbalazsworks/smartscrumpokerbackend/SmartScrumPokerApplication.java
@@ -0,0 +1,13 @@
+package com.kbalazsworks.smartscrumpokerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartScrumPokerApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SmartScrumPokerApplication.class, args);
+    }
+}
---
+++ src/main/java/com/kbalazsworks/smartscrumpokerbackend/controllers/index/GetIndexController.java
@@ -0,0 +1,17 @@
+package com.kbalazsworks.smartscrumpokerbackend.controllers.index;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController("AccountGetByReviewIdAction")
+@RequestMapping("/")
+public class GetIndexController
+{
+    @GetMapping("/")
+    public String action()
+    {
+        return "lofasz";
+    }
+}
