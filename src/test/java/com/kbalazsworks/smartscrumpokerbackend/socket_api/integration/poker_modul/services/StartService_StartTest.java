34731d66341315ffc7cfb2286ebd9d0816d1e19d
Balazs Krizsan <krizsan.balazs@gmail.com>
2024-03-14 00:27:49 +0100

    jooq binaries and gen config

---
+++ src/test/java/com/kbalazsworks/smartscrumpokerbackend/socket_api/integration/poker_modul/services/StartService_StartTest.java
@@ -0,0 +1,12 @@
+package com.kbalazsworks.smartscrumpokerbackend.socket_api.integration.poker_modul.services;
+
+import org.junit.jupiter.api.Test;
+
+public class StartService_StartTest
+{
+    @Test
+    public void test()
+    {
+
+    }
+}
