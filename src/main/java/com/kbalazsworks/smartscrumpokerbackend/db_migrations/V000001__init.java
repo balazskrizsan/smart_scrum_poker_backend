08ddee113c9e7183a6cc91d0552606af7f69ded5
Balazs Krizsan <krizsan.balazs@gmail.com>
2024-03-14 00:29:23 +0100

    Migration setup

---
+++ src/main/java/com/kbalazsworks/smartscrumpokerbackend/db_migrations/V000001__init.java
@@ -0,0 +1,27 @@
+package com.kbalazsworks.smartscrumpokerbackend.db_migrations;
+
+import lombok.val;
+import org.flywaydb.core.api.migration.Context;
+import org.springframework.stereotype.Component;
+
+import static org.jooq.impl.DSL.constraint;
+import static org.jooq.impl.SQLDataType.*;
+
+@Component
+public class V000001__init extends AbstractBaseJooqMigration
+{
+    @Override
+    public void migrate(Context context)
+    {
+        val qB = getQueryBuilder(context);
+
+        qB.createTable("pokers")
+            .column("id", BIGINT.nullable(false).identity(true))
+            .column("id_secure", UUID.nullable(false))
+            .column("name", VARCHAR.nullable(false))
+            .column("created_at", TIMESTAMP.nullable(false))
+            .column("created_by", CHAR(36).nullable(true))
+            .constraint(constraint("pokers_pk").primaryKey("id"))
+            .execute();
+    }
+}
