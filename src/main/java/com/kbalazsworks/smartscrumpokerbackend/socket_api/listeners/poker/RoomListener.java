46660a5866cfb26353f8910bb6e186d85c5575f5
Balazs Krizsan <krizsan.balazs@gmail.com>
2024-03-27 00:15:50 +0100

    Naming correction

---
+++ src/main/java/com/kbalazsworks/smartscrumpokerbackend/socket_api/listeners/poker/RoomListener.java
@@ -0,0 +1,32 @@
+package com.kbalazsworks.smartscrumpokerbackend.socket_api.listeners.poker;
+
+import com.kbalazsworks.smartscrumpokerbackend.api.builders.ResponseEntityBuilder;
+import com.kbalazsworks.smartscrumpokerbackend.api.exceptions.ApiException;
+import com.kbalazsworks.smartscrumpokerbackend.socket_api.enums.SocketDestination;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Log4j2
+@Controller
+@RequiredArgsConstructor
+public class RoomListener
+{
+    private final SimpMessagingTemplate template;
+
+    @MessageMapping("/poker-room-{pokerIdSecure}")
+    public void roomHandler(@Payload String message, @DestinationVariable String pokerIdSecure)
+        throws ApiException
+    {
+        log.info("Listener:/poker-room-pokerIdSecure/{}: {}", message, pokerIdSecure);
+
+        template.convertAndSend(
+            "/queue/reply-" + pokerIdSecure,
+            new ResponseEntityBuilder<String>().socketDestination(SocketDestination.POKER_START).data(message).build()
+        );
+    }
+}
