5780b2667a0aeb02ac88f3eebf73629d1b107af7
Balazs Krizsan <krizsan.balazs@gmail.com>
2024-04-04 17:05:49 +0200

    RequestMapperService refact with time

--- src/main/java/com/kbalazsworks/smartscrumpokerbackend/socket_api/services/RequestMapperService.java
+++ src/main/java/com/kbalazsworks/smartscrumpokerbackend/socket_api/services/RequestMapperService.java
@@ -8,28 +8,34 @@ import com.kbalazsworks.smartscrumpokerbackend.socket_domain.poker_module.entiti
 import com.kbalazsworks.smartscrumpokerbackend.socket_domain.poker_module.entities.Ticket;
 import com.kbalazsworks.smartscrumpokerbackend.socket_domain.poker_module.entities.Vote;
 import com.kbalazsworks.smartscrumpokerbackend.socket_domain.poker_module.value_objects.StartPoker;
+import lombok.NonNull;
 
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
-import java.util.UUID;
 
 public class RequestMapperService
 {
-    public static StartPoker mapToEntity(StartRequest request)
+    public static LocalDateTime getNow()
+    {
+        return new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
+
+    public static StartPoker mapToEntity(@NonNull StartRequest request)
     {
         return new StartPoker(
             new Poker(
                 null,
                 null,
                 request.sprintTitle(),
-                new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime(),
+                getNow(),
                 request.starterInsecureUserId()
             ),
             request.ticketNames().stream().map(tn -> new Ticket(null, null, null, tn, false)).toList()
         );
     }
 
-    public static Vote mapToEntity(VoteRequest voteRequest)
+    public static Vote mapToEntity(@NonNull VoteRequest voteRequest)
     {
         return new Vote(
             null,
@@ -38,18 +44,18 @@ public class RequestMapperService
             voteRequest.voteComplexity(),
             voteRequest.voteEffort(),
             null,
-            new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime(),
+            getNow(),
             voteRequest.userIdSecure()
         );
     }
 
-    public static InsecureUser mapToEntity(InsecureUserCreateRequest insecureUserCreateRequest)
+    public static InsecureUser mapToEntity(@NonNull InsecureUserCreateRequest insecureUserCreateRequest)
     {
         return new InsecureUser(
             null,
             null,
             insecureUserCreateRequest.userName(),
-            new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()
+            getNow()
         );
     }
 }
