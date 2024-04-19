a96899e351770680084f1294b738554943c67c73
Balazs Krizsan <krizsan.balazs@gmail.com>
2024-04-19 09:43:13 +0200

    CS: NotificationService

--- src/main/java/com/kbalazsworks/smartscrumpokerbackend/socket_api/services/NotificationService.java
+++ src/main/java/com/kbalazsworks/smartscrumpokerbackend/socket_api/services/NotificationService.java
@@ -3,6 +3,7 @@ package com.kbalazsworks.smartscrumpokerbackend.socket_api.services;
 import com.kbalazsworks.smartscrumpokerbackend.api.builders.ResponseEntityBuilder;
 import com.kbalazsworks.smartscrumpokerbackend.api.exceptions.ApiException;
 import com.kbalazsworks.smartscrumpokerbackend.socket_api.enums.SocketDestination;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,12 @@ public class NotificationService
 {
     private final SimpMessagingTemplate template;
 
-    public <T> void notifyPokerRoom(UUID pokerIdSecure, T data, SocketDestination socketDestination) throws ApiException
+    public <T> void notifyPokerRoom(
+        @NonNull UUID pokerIdSecure,
+        @NonNull T data,
+        @NonNull SocketDestination socketDestination
+    )
+        throws ApiException
     {
         template.convertAndSend(
             STR."/queue/reply-\{pokerIdSecure}",
