2a949ad0a68d81266a8ae6ddd6d84506915fddb9
Balazs Krizsan <krizsan.balazs@gmail.com>
2024-05-21 11:37:10 +0200

    ResponseData clean

--- src/main/java/com/kbalazsworks/smartscrumpokerbackend/api/value_objects/ResponseData.java
+++ src/main/java/com/kbalazsworks/smartscrumpokerbackend/api/value_objects/ResponseData.java
@@ -1,14 +1,11 @@
 package com.kbalazsworks.smartscrumpokerbackend.api.value_objects;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
-public class ResponseData<T>
+public record ResponseData<T>(
+    T data,
+    Boolean success,
+    int errorCode,
+    String requestId,
+    String socketResponseDestination
+)
 {
-    private final T       data;
-    private final Boolean success;
-    private final int     errorCode;
-    private final String  requestId;
 }
